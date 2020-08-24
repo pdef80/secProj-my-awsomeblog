786637b4031240846f8ab7b7580b4f134b8558c8
Denis <pdef8085@gmail.com>
2020-08-24 20:58:30 +0300

    Make Post dir admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=300)
+    post_date = models.DateTimeField(auto_now_add=True)
+    post_text = models.TextField()
+    post_image = models.ImageField(upload_to='post_images/')
+
